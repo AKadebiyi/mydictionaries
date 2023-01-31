dc8b7e70b6e77a9e244eac344a31cda6ccc063dd
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-01-31 15:01:47 -0600

    adeola worked on exercises 2, 3 and 4

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,22 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+# print(person)
+
+# print out the name of the 2nd child
+print(person["children"][1])
+
+# print out the name of the cat
+print(person["pets"]["cat"])
+
+# use a loop to print out the names of each child
+for x in person["children"]:
+    print(x)
+
+
+# use a loop to print out the pets in the ffg format:
+pets_dict = person["pets"]
+
+for key, value in pets_dict.items():  # OR
+    print(f"the type of pet is: {key} and the name of the pet is: {value}")
